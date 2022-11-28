36c4cdda18c54378688b88e5b8f26eb75efcb4a7
kverthez <92855389+kverthez@users.noreply.github.com>
2022-11-28 08:55:17 +0000

    part1: Get familiar with Jest

--- frontend/src/components/add-todo/add-todo.test.tsx
+++ frontend/src/components/add-todo/add-todo.test.tsx
@@ -4,3 +4,16 @@ import { screen, render, waitFor } from "../../test-lib/test-utils";
 test("dummy test", () => {
     expect(1 + 2).toEqual(3);
 });
+
+test("when passing an empty string, isValid should return false", () =>{
+    expect(isValid("")).toBeFalsy();
+
+});
+
+test("when passing a string with just whitespace, isValid should return false", ()=>{
+    expect(isValid(" ")).toBeFalsy();
+});
+
+test("when passing a string with text, isValid should return true", ()=>{
+    expect(isValid("test1")).toBeTruthy();
+});
