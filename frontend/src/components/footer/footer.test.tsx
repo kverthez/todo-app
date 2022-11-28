5e61159e081d8103c48c4484ccb97d3ff07f2475
kverthez <92855389+kverthez@users.noreply.github.com>
2022-11-28 09:47:13 +0000

    part3: Testing Library

--- frontend/src/components/footer/footer.test.tsx
+++ frontend/src/components/footer/footer.test.tsx
@@ -29,3 +29,26 @@ it("when passing an array where 1 todo is done and 2 are not done, countTodosLef
     expect(countTodosLeft(todos)).toEqual(2);
 });
 
+it("'All done!' when there are no todos left", ()=>{
+    const exampleTodos = [aTodo({ done: true })];
+    render(<Footer todos={exampleTodos} />);
+
+    //screen.logTestingPlaygroundURL();
+    screen.getByText(/all done/i);
+});
+
+it("'1 todo left' when there is one", () =>{
+    const exampleTodos = [aTodo({ done: false })];
+    render(<Footer todos={exampleTodos} />);
+    screen.getByText('1 todo left');
+});
+
+
+it("'4 todos left' when there are multiple", () =>{
+    const exampleTodos = [aTodo({ done: false }),
+        aTodo({ done: false }),
+        aTodo({ done: false }),
+        aTodo({ done: false })];
+    render(<Footer todos={exampleTodos} />);
+    screen.getByText('4 todos left');
+});
