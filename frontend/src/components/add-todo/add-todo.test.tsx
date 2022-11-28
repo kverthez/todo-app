3633a330c183bd0a5bcd91fd9b53899b11f2f982
kverthez <92855389+kverthez@users.noreply.github.com>
2022-11-28 10:10:39 +0000

    4. Simulating the user

--- frontend/src/components/add-todo/add-todo.test.tsx
+++ frontend/src/components/add-todo/add-todo.test.tsx
@@ -1,4 +1,4 @@
-import { isValid } from "./index";
+import { AddTodoForm, isValid } from "./index";
 import { screen, render, waitFor } from "../../test-lib/test-utils";
 
 test("dummy test", () => {
@@ -16,4 +16,21 @@ test("when passing a string with just whitespace, isValid should return false", 
 
 test("when passing a string with text, isValid should return true", ()=>{
     expect(isValid("test1")).toBeTruthy();
+});
+
+it("when pressing enter description should become empty", async()=>{
+    const { user } = render(<AddTodoForm />);
+    const field = screen.getByRole('textbox', {name: /description/i});
+    await user.type(field, "test1{Enter}");
+    await waitFor(()=>expect(field).toHaveValue(""));
+});
+
+it("when clicking on addButton description should become empty", async() =>{
+    const { user } = render(<AddTodoForm />);
+    //screen.logTestingPlaygroundURL();
+    const field = screen.getByRole('textbox', {name: /description/i});
+    await user.type(field, "test1");
+    const btnAdd = screen.getByRole('button', {name: /add/i});
+    await user.click(btnAdd);
+    await waitFor(()=>expect(field).toHaveValue(""));
 });
